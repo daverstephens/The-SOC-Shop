93d676fcd93c689ab0e0ce70f93778da5d4b5aa9
daverstephens <daverstephens@gmail.com>
2015-02-06 14:40:02 +0000

    Update MWiki-Post-Install-0.1.sh

--- MWiki-Stuff/MWiki-Post-Install-0.1.sh
+++ MWiki-Stuff/MWiki-Post-Install-0.1.sh
@@ -33,15 +33,15 @@ EOF
 # that lets you store and query data within the wiki's pages.
 	php -r "readfile('https://getcomposer.org/installer');" | php
 	php composer.phar require mediawiki/semantic-media-wiki "~2.0"
-	php maintenance/update.php
+	php /var/www/html/mediawiki/maintenance/update.php
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 enableSemantics( '172.21.1.1' );
 EOF
 # Install 'Admin Links' - Extension to MediaWiki that defines a special page,
 # "Special:AdminLinks" that holds links meant to be helpful for wiki administrators
-cd /var/www/mediawiki/extensions
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/AdminLinks.git
+	cd /var/www/html/mediawiki/extensions
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/AdminLinks.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 require_once( "$IP/extensions/AdminLinks/AdminLinks.php" );
@@ -49,7 +49,7 @@ EOF
 # Install 'Replace Text' - extension to MediaWiki that provides a special page
 # to allow administrators to do a global string find-and-replace on both the 
 # text and titles of the wiki's content pages.
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ReplaceText.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ReplaceText.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 require_once( "$IP/extensions/ReplaceText/ReplaceText.php" );
@@ -58,7 +58,7 @@ EOF
 # and format structured data from one or more sources. These sources can include
 # external URLs, regular wiki pages, uploaded files, files on the local server
 # databases or LDAP directories.
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ExternalData.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ExternalData.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 include_once( "$IP/extensions/ExternalData/ExternalData.php" );
@@ -66,14 +66,14 @@ EOF
 # Install 'Semantic Drilldown' - extension to MediaWiki that provides a page 
 # for drilling down through a site's data, using categories and filters on semantic
 # properties.
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticDrilldown.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticDrilldown.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 include_once( "$IP/extensions/SemanticDrilldown/SemanticDrilldown.php" );
 EOF
 # Install 'Semantic Forms is an extension to MediaWiki that allows users to add
 # edit and query data using forms.
-git clone https://git.wikimedia.org/git/mediawiki/extensions/SemanticForms.git
+	git clone https://git.wikimedia.org/git/mediawiki/extensions/SemanticForms.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 include_once( "$IP/extensions/SemanticForms/SemanticForms.php" );
@@ -81,7 +81,7 @@ EOF
 # Install 'Semantic Compound Queries' - extension to MediaWiki, meant to work
 # with Semantic MediaWiki, that allows for the display of more than one SMW 
 # inline query in one results display set.
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticCompoundQueries.git
+	git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticCompoundQueries.git
 # Update localsettings.php
 	cat >> /var/www/html/mediawiki/LocalSettings.php <<EOF
 require_once( "$IP/extensions/SemanticCompoundQueries/SemanticCompoundQueries.php" );
