262f161b3de6d76a80aac3091258939eaeeebe86
daverstephens <daverstephens@gmail.com>
2017-09-25 14:07:07 +0100

    Update Spiderfoot_install.sh

--- Threat_Intel/Spiderfoot_install.sh
+++ Threat_Intel/Spiderfoot_install.sh
@@ -3,7 +3,7 @@ sudo apt-get upgrade
 sudo apt-get install python-pip libssl-dev
 sudo pip install lxml netaddr M2Crypto cherrypy mako requests bs4
 sudo apt-get install python-m2crypto
-wget -O spiderfoot.tar.gz https://downloads.sourceforge.net/project/spiderfoot/spiderfoot-2.10-src.tar.gz?r=http%3A%2F%2Fwww.spiderfoot.net%2Fdownload%2F&ts=1501506625&use_mirror=kent
+wget -O spiderfoot.tar.gz https://github.com/smicallef/spiderfoot/archive/master.zip
 tar xvf spiderfoot.tar.gz
 rm spiderfoot.tar.gz
 cd spider*
