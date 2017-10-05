d4816e4341091cf2a4aae6455240ada8bb51a810
daverstephens <daverstephens@gmail.com>
2017-10-05 15:52:06 +0100

    Update Install_GScraper.sh

--- Threat_Intel/Install_GScraper.sh
+++ Threat_Intel/Install_GScraper.sh
@@ -1,21 +1,14 @@
 #!/usr/bin/env bash
-apt-get install curl python-pip selenium
+apt-get install curl python-pip
+pip install wget selenium
 
 # Versions
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-SELENIUM_STANDALONE_VERSION=3.4.0
-SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
 rm ~/google-chrome-stable_current_amd64.deb
-rm ~/selenium-server-standalone-*.jar
 rm ~/chromedriver_linux64.zip
 sudo rm /usr/local/bin/chromedriver
-sudo rm /usr/local/bin/selenium-server-standalone.jar
-
-# Install dependencies.
-sudo apt-get update
-sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome.
 wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
@@ -30,9 +23,3 @@ rm ~/chromedriver_linux64.zip
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
 sudo chown root:root /usr/local/bin/chromedriver
 sudo chmod 0755 /usr/local/bin/chromedriver
-
-# Install Selenium.
-wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
-sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
