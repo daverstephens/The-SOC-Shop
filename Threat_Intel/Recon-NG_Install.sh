201667b6f8070dbc4bef099b2fbc752d61bfc42f
daverstephens <daverstephens@gmail.com>
2017-10-09 09:31:04 +0100

    Create Recon-NG_Install.sh

---
+++ Threat_Intel/Recon-NG_Install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install git python-pip python-dnspython python-mechanize python-slowaes python-xlsxwriter python-jsonrpclib python-lxml
+pip install dicttoxml --upgrade
+git clone https://LaNMaSteR53@bitbucket.org/LaNMaSteR53/recon-ng.git
+cd recon-ng
+./recon-ng
